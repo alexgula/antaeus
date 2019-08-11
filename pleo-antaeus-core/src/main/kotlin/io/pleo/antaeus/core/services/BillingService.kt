360e1da7edaea87e79de3044ec5ab0102b353c24
Oleksandr Gula <olgua@vestas.com>
2019-08-11 17:34:35 +0200

    Add tests and implementation for multiple invoices

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,16 +1,19 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
-import io.pleo.antaeus.models.Currency
-import io.pleo.antaeus.models.Invoice
-import io.pleo.antaeus.models.InvoiceStatus
-import io.pleo.antaeus.models.Money
-import java.math.BigDecimal
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+        private val paymentProvider: PaymentProvider,
+        private val invoiceService: InvoiceService
 ) {
+    /**
+     * Charges all available invoices
+     *
+     * @return true if there are no pending invoices left, false otherwise
+     */
     fun chargeAll(): Boolean {
-        return paymentProvider.charge(Invoice(0, 0, Money(BigDecimal.valueOf(0), Currency.EUR), InvoiceStatus.PAID))
+        return invoiceService.fetchAll()
+                .map { paymentProvider.charge(it) }
+                .all { it }
     }
 }
--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -3,17 +3,32 @@ package io.pleo.antaeus.core.services
 import io.mockk.every
 import io.mockk.mockk
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.models.Money
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class BillingServiceTest {
     private val paymentProvider = mockk<PaymentProvider>()
 
-    private val billingService = BillingService(paymentProvider = paymentProvider)
+    private val invoiceService = mockk<InvoiceService>()
+
+    private val billingService = BillingService(paymentProvider = paymentProvider, invoiceService = invoiceService)
+
+    @Test
+    fun `returns true if no invoices`() {
+        every { invoiceService.fetchAll() } returns listOf()
+        val result = billingService.chargeAll()
+        Assertions.assertEquals(true, result)
+    }
 
     @Test
     fun `returns true if payment provider is succeeding`() {
         every { paymentProvider.charge(any()) } returns true
+        every { invoiceService.fetchAll() } returns listOf(invoice())
         val result = billingService.chargeAll()
         Assertions.assertEquals(true, result)
     }
@@ -21,7 +36,18 @@ class BillingServiceTest {
     @Test
     fun `returns false if payment provider is failing`() {
         every { paymentProvider.charge(any()) } returns false
+        every { invoiceService.fetchAll() } returns listOf(invoice())
         val result = billingService.chargeAll()
         Assertions.assertEquals(false, result)
     }
+
+    @Test
+    fun `returns false if payment provider is failing sometimes`() {
+        every { paymentProvider.charge(any()) } returns true andThen false
+        every { invoiceService.fetchAll() } returns listOf(invoice(), invoice())
+        val result = billingService.chargeAll()
+        Assertions.assertEquals(false, result)
+    }
+
+    private fun invoice() = Invoice(0, 0, Money(BigDecimal.valueOf(0), Currency.EUR), InvoiceStatus.PAID)
 }
